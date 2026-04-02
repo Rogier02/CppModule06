a6aba36e4b2c6481b30f6b8be7957e87400aa386
Rogier02 <rogier.goossens@gmail.com>
2026-04-02 18:58:52 +0200

    start on next ex

---
+++ ex01/include/Data.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+struct Data {
+	std::string name;
+	std::string vocation;
+	int			age;
+	bool		vegan;
+};
---
+++ ex01/include/Serializer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+#include "Data.hpp"
+
+class Serializer {
+	private:
+		Serializer();
+		Serializer(const Serializer& other);
+		~Serializer();
+		Serializer& operator=(const Serializer& other);
+	
+	public:
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
+};
